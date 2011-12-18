0c1ac80d15fe859a5c6fde0cab9a69ffbdbe6884
Colin Ian King <colin.king@canonical.com>
2011-12-18 12:23:06 +0000

    Add timer info cache, more memory efficient
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- eventstat.c
+++ eventstat.c
@@ -29,18 +29,99 @@
 
 #define TABLE_SIZE	(251)		/* Should be a prime */
 
-typedef struct timer_stat {
-	unsigned long	count;		/* Number of events */
+typedef struct {
 	pid_t		pid;
 	char 		*task;		/* Name of process/kernel task */
 	char		*func;		/* Kernel waiting func */
 	char		*timer;		/* Kernel timer */
 	char		*ident;		/* Unique identity */
+} timer_info_t;
+
+typedef struct timer_stat {
+	unsigned long	count;		/* Number of events */
 	unsigned long	delta;		/* Change in events since last time */
 	bool		old;		/* Existing event, not a new one */
+	timer_info_t	*info;		/* Timer info */
 	struct timer_stat *next;	/* Next timer stat in hash table */
-	struct timer_stat *sorted_next;	/* Next timer stat in sorted list */
+	struct timer_stat *sorted_freq_next;	/* Next timer stat in event frequency sorted list */
+	struct timer_stat *sorted_ident_next;	/* Next timer stat in ident sorted list */
 } timer_stat_t;
+
+typedef struct timer_info_item {
+	timer_info_t		info;
+	struct timer_info_item	*next;
+} timer_info_item_t;
+
+typedef struct {
+	timer_info_item_t	*head;
+	timer_info_item_t	*tail;
+} timer_info_list_t;
+
+timer_info_list_t timer_info_list;	/* cache list of timer_info */
+
+/*
+ *  timer_info_find()
+ *	try to find existing timer info in cache, and to the cache
+ *	if it is new.
+ */
+timer_info_t *timer_info_find(timer_info_t *info)
+{
+	timer_info_item_t *item = timer_info_list.head;
+
+	while (item) {
+		if (strcmp(info->ident, item->info.ident) == 0) 
+			return &item->info;
+		item = item->next;
+	}
+	if ((item = calloc(1, sizeof(timer_info_item_t))) == NULL) {
+		fprintf(stderr, "Cannot allocate timer info\n");
+		exit(EXIT_FAILURE);
+	}
+
+	item->info.pid = info->pid;
+	item->info.task = strdup(info->task);
+	item->info.func = strdup(info->func);
+	item->info.timer = strdup(info->timer);
+	item->info.ident = strdup(info->ident);
+
+	if (item->info.task == NULL ||
+	    item->info.func == NULL ||
+	    item->info.timer == NULL ||
+	    item->info.ident == NULL) {
+		fprintf(stderr, "Out of memory allocating a timer stat fields\n");
+		exit(1);
+	}
+
+	/* Does not exist in list, append it */
+
+	if (timer_info_list.head == NULL) {
+		timer_info_list.head = item;
+		timer_info_list.tail = item;
+	} else {
+		timer_info_list.tail->next = item;
+		timer_info_list.tail = item;
+	}
+	return &item->info;
+}
+
+/*
+ *  timer_info_free
+ *	free up all unique timer infos
+ */
+void timer_info_free(void)
+{
+	timer_info_item_t *item = timer_info_list.head;
+
+	while (item) {
+		timer_info_item_t *next = item->next;
+		free(item->info.task);
+		free(item->info.func);
+		free(item->info.timer);
+		free(item->info.ident);
+		free(item);
+		item = next;
+	}
+}
 
 /*
  *  hash_pjw()
@@ -76,11 +157,6 @@ void timer_stat_free_contents(
 
 		while (ts) {
 			timer_stat_t *next = ts->next;
-
-			free(ts->task);
-			free(ts->func);
-			free(ts->timer);
-			free(ts->ident);
 			free(ts);
 
 			ts = next;
@@ -105,6 +181,7 @@ void timer_stat_add(
 	char buf[4096];
 	timer_stat_t *ts;
 	timer_stat_t *ts_new;
+	timer_info_t info;
 	unsigned long h;
 
 	snprintf(buf, sizeof(buf), "%d:%s:%s:%s", pid, task, func, timer);
@@ -112,7 +189,7 @@ void timer_stat_add(
 	ts = timer_stats[h];
 
 	while (ts != NULL) {
-		if (strcmp(ts->ident, buf) == 0) {
+		if (strcmp(ts->info->ident, buf) == 0) {
 			ts->count += count;
 			return;
 		}
@@ -125,22 +202,19 @@ void timer_stat_add(
 		exit(1);
 	}
 
-	ts_new->count  = count;
-	ts_new->pid    = pid;
-	ts_new->task   = strdup(task);
-	ts_new->func   = strdup(func);
-	ts_new->timer  = strdup(timer);
-	ts_new->ident  = strdup(buf);
-	ts_new->next   = timer_stats[h];
-	ts_new->sorted_next = NULL;
+	info.pid = pid;
+	info.task = task;
+	info.func = func;
+	info.timer = timer;
+	info.ident = buf;
 
-	if (ts_new->task == NULL ||
-	    ts_new->func == NULL ||
-	    ts_new->timer == NULL ||
-	    ts_new->ident == NULL) {
-		fprintf(stderr, "Out of memory allocating a timer stat fields\n");
-		exit(1);
-	}
+
+	ts_new->count  = count;
+	ts_new->info = timer_info_find(&info);
+	ts_new->next  = timer_stats[h];
+	ts_new->sorted_freq_next = NULL;
+	ts_new->sorted_ident_next = NULL;
+
 
 	timer_stats[h] = ts_new;
 }
@@ -157,29 +231,48 @@ timer_stat_t *timer_stat_find(
 	char buf[4096];
 
 	snprintf(buf, sizeof(buf), "%d:%s:%s:%s",
-		needle->pid, needle->task, needle->func, needle->timer);
+		needle->info->pid, needle->info->task,
+		needle->info->func, needle->info->timer);
 
 	for (ts = haystack[hash_pjw(buf)]; ts; ts = ts->next) {
-		if (strcmp(ts->ident, buf) == 0)
+		if (strcmp(ts->info->ident, buf) == 0)
 			return ts;
 	}
 	return NULL;	/* no success */
 }
 
 /*
- *  timer_stat_sort_add()
+ *  timer_stat_sort_freq_add()
  *	add a timer stat to a sorted list of timer stats
  */
-void timer_stat_sort_add(
+void timer_stat_sort_freq_add(
 	timer_stat_t **sorted,		/* timer stat sorted list */
 	timer_stat_t *new)		/* timer stat to add */
 {
 	while (*sorted != NULL) {
 		if ((*sorted)->delta < new->delta) {
-			new->sorted_next = *(sorted);
+			new->sorted_freq_next = *(sorted);
 			break;
 		}
-		sorted = &(*sorted)->sorted_next;
+		sorted = &(*sorted)->sorted_freq_next;
+	}
+	*sorted = new;
+}
+
+/*
+ *  timer_stat_sort_ident_add()
+ *	add a timer stat to a sorted list of timer stats
+ */
+void timer_stat_sort_ident_add(
+	timer_stat_t **sorted,		/* timer stat sorted list */
+	timer_stat_t *new)		/* timer stat to add */
+{
+	while (*sorted != NULL) {
+		if (strcmp((*sorted)->info->ident, new->info->ident) < 0) {
+			new->sorted_ident_next = *(sorted);
+			break;
+		}
+		sorted = &(*sorted)->sorted_ident_next;
 	}
 	*sorted = new;
 }
@@ -191,8 +284,8 @@ void timer_stat_sort_add(
  *	silently die
  */
 void timer_stat_diff(
-	int duration,
-	int n_lines,
+	const int duration,
+	const int n_lines,
 	timer_stat_t *timer_stats_old[],
 	timer_stat_t *timer_stats_new[],
 	unsigned long *total)
@@ -211,12 +304,12 @@ void timer_stat_diff(
 				ts->delta = ts->count - found->count;
 				if (ts->delta) {
 					ts->old = true;
-					timer_stat_sort_add(&sorted, ts);
+					timer_stat_sort_freq_add(&sorted, ts);
 				}
 			} else {
 				ts->delta = 0;
 				ts->old = false;
-				timer_stat_sort_add(&sorted, ts);
+				timer_stat_sort_freq_add(&sorted, ts);
 			}
 			ts = ts->next;
 		}
@@ -232,11 +325,11 @@ void timer_stat_diff(
 			printf("%1s %6.2f %5d %-15s %-25s %-s\n",
 				sorted->old ? " " : "N",
 				(double)sorted->delta / duration,
-				sorted->pid, sorted->task,
-				sorted->func, sorted->timer);
+				sorted->info->pid, sorted->info->task,
+				sorted->info->func, sorted->info->timer);
 		}
 		*total += sorted->delta;
-		sorted = sorted->sorted_next;
+		sorted = sorted->sorted_freq_next;
 	}
 }
 
@@ -414,6 +507,8 @@ int main(int argc, char **argv)
 	timer_stat_free_contents(timer_stats_new);
 	free(timer_stats_old);
 	free(timer_stats_new);
+
+	timer_info_free();
 
 	set_timer_stat("0");
 

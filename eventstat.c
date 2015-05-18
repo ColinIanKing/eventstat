ca31eef356b68f8719a9fe27bed87d67ffa453c1
Colin Ian King <colin.king@canonical.com>
2015-05-18 18:20:31 +0100

    Remove signal handlers for SIGBUS, SIGABRT, SIGILL
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- eventstat.c
+++ eventstat.c
@@ -133,12 +133,6 @@ static const int signals[] = {
 #ifdef SIGQUIT
 	SIGQUIT,
 #endif
-#ifdef SIGILL
-	SIGILL,
-#endif
-#ifdef SIGABRT
-	SIGABRT,
-#endif
 #ifdef SIGFPE
 	SIGFPE,
 #endif
@@ -154,9 +148,6 @@ static const int signals[] = {
 #ifdef SIGUSR2
 	SIGUSR2,
 	/* POSIX.1-2001 */
-#endif
-#ifdef SIGBUS
-	SIGBUS,
 #endif
 #ifdef SIGXCPU
 	SIGXCPU,

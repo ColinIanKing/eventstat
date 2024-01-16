ColinIanKing/eventstat

commit 288689493f898f039d6691734cda4daaa6dac796

Makefile: use xz compression on make dist rule
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

Makefile
@@ -46,7 +46,7 @@ dist:
 	mkdir eventstat-$(VERSION)
 	cp -rp Makefile eventstat.c eventstat.8 COPYING snap .travis.yml \
 		bash-completion README.md eventstat-$(VERSION)
-	tar -zcf eventstat-$(VERSION).tar.gz eventstat-$(VERSION)
+	tar -Jcf eventstat-$(VERSION).tar.xz eventstat-$(VERSION)
 	rm -rf eventstat-$(VERSION)
 
 clean:

commit de7869950616040c426e8df988d629b2608afce3

Makefile: remove .xz tarball and not .gz
Fixes: 288689493f89 ("Makefile: use xz compression on make dist rule")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

Makefile
@@ -51,7 +51,7 @@ dist:
 
 clean:
 	rm -f eventstat eventstat.o eventstat.8.gz
-	rm -f eventstat-$(VERSION).tar.gz
+	rm -f eventstat-$(VERSION).tar.xz
 
 install: eventstat eventstat.8.gz
 	mkdir -p ${DESTDIR}${BINDIR}